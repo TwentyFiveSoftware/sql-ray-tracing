e94ec085df3bb021d0d43869c40f21402877cdc7
TwentyFive <twentyfivesoftware@gmail.com>
2025-04-03 18:55:39 +0200

    Performance optimization for sphere intersection calculation

--- ray-tracing.sql
+++ ray-tracing.sql
@@ -44,57 +44,51 @@ WITH
                CAST(y AS FLOAT) / (@HEIGHT - 1)
         FROM pixel_coordinates
     ),
-    not_normalized_rays (ray_id, pixel_x, pixel_y, origin_x, origin_y, origin_z, direction_x, direction_y, direction_z)
-        AS (
-        SELECT y * @WIDTH + x,
-               x,
-               y,
-               @CAMERA_ORIGIN_X,
-               @CAMERA_ORIGIN_Y,
-               @CAMERA_ORIGIN_Z,
-               @CAMERA_UPPER_LEFT_CORNER_X + @VIEWPORT_WIDTH * u,
-               @CAMERA_UPPER_LEFT_CORNER_Y + @VIEWPORT_HEIGHT * v,
-               @CAMERA_UPPER_LEFT_CORNER_Z
-        FROM pixel_uv
-    ),
-    rays (ray_id, pixel_x, pixel_y, origin_x, origin_y, origin_z, direction_x, direction_y, direction_z) AS (
+    rays (ray_id, pixel_x, pixel_y, ray_origin_x, ray_origin_y, ray_origin_z, ray_direction_x, ray_direction_y,
+          ray_direction_z) AS (
         SELECT ray_id,
                pixel_x,
                pixel_y,
-               origin_x,
-               origin_y,
-               origin_z,
-               direction_x / SQRT(direction_x * direction_x + direction_y * direction_y + direction_z * direction_z),
-               direction_y / SQRT(direction_x * direction_x + direction_y * direction_y + direction_z * direction_z),
-               direction_z / SQRT(direction_x * direction_x + direction_y * direction_y + direction_z * direction_z)
-        FROM not_normalized_rays
-    ),
-    ray_sphere_collisions_discriminant_calc (ray_id, sphere_id, a, half_b, c) AS (
-        SELECT ray_id,
-               sphere_id,
-               -- a:
-               rays.direction_x * rays.direction_x + rays.direction_y * rays.direction_y +
-               rays.direction_z * rays.direction_z,
-               -- half_b:
-               (rays.origin_x - spheres.center_x) * rays.direction_x +
-               (rays.origin_y - spheres.center_y) * rays.direction_y +
-               (rays.origin_z - spheres.center_z) * rays.direction_z,
-               -- c:
-               ((rays.origin_x - spheres.center_x) * (rays.origin_x - spheres.center_x) +
-                (rays.origin_y - spheres.center_y) * (rays.origin_y - spheres.center_y) +
-                (rays.origin_z - spheres.center_z) * (rays.origin_z - spheres.center_z)) -
-               spheres.radius * spheres.radius
-        FROM rays,
-             spheres
+               ray_origin_x,
+               ray_origin_y,
+               ray_origin_z,
+               ray_direction_x / SQRT(ray_direction_x * ray_direction_x + ray_direction_y * ray_direction_y +
+                                      ray_direction_z * ray_direction_z),
+               ray_direction_y / SQRT(ray_direction_x * ray_direction_x + ray_direction_y * ray_direction_y +
+                                      ray_direction_z * ray_direction_z),
+               ray_direction_z / SQRT(ray_direction_x * ray_direction_x + ray_direction_y * ray_direction_y +
+                                      ray_direction_z * ray_direction_z)
+        FROM (
+            SELECT y * @WIDTH + x                                     AS ray_id,
+                   x                                                  AS pixel_x,
+                   y                                                  AS pixel_y,
+                   @CAMERA_ORIGIN_X                                   AS ray_origin_x,
+                   @CAMERA_ORIGIN_Y                                   AS ray_origin_y,
+                   @CAMERA_ORIGIN_Z                                   AS ray_origin_z,
+                   @CAMERA_UPPER_LEFT_CORNER_X + @VIEWPORT_WIDTH * u  AS ray_direction_x,
+                   @CAMERA_UPPER_LEFT_CORNER_Y + @VIEWPORT_HEIGHT * v AS ray_direction_y,
+                   @CAMERA_UPPER_LEFT_CORNER_Z                        AS ray_direction_z
+            FROM pixel_uv
+        ) AS not_normalized_rays
     ),
     ray_sphere_collisions_discriminant (ray_id, sphere_id, a, half_b, c, discriminant) AS (
-        SELECT ray_id,
-               sphere_id,
-               a,
-               half_b,
-               c,
+        SELECT *,
                half_b * half_b - a * c
-        FROM ray_sphere_collisions_discriminant_calc
+        FROM (
+            SELECT ray_id,
+                   sphere_id,
+                   ray_direction_x * ray_direction_x + ray_direction_y * ray_direction_y +
+                   ray_direction_z * ray_direction_z                   AS a,
+                   (ray_origin_x - spheres.center_x) * ray_direction_x +
+                   (ray_origin_y - spheres.center_y) * ray_direction_y +
+                   (ray_origin_z - spheres.center_z) * ray_direction_z AS half_b,
+                   ((ray_origin_x - spheres.center_x) * (ray_origin_x - spheres.center_x) +
+                    (ray_origin_y - spheres.center_y) * (ray_origin_y - spheres.center_y) +
+                    (ray_origin_z - spheres.center_z) * (ray_origin_z - spheres.center_z)) -
+                   spheres.radius * spheres.radius                     AS c
+            FROM rays,
+                 spheres
+        ) AS ray_sphere_collisions_discriminant_calc
     ),
     ray_sphere_collisions_roots (ray_id, sphere_id, root_1, root_2) AS (
         SELECT ray_id,
@@ -114,7 +108,7 @@ WITH
                    END
         FROM ray_sphere_collisions_roots
     ),
-    ray_collisions (ray_id, sphere_id, t) AS (
+    ray_collisions_closest (ray_id, sphere_id, t) AS (
         SELECT ray_sphere_collisions_t.ray_id,
                sphere_id,
                ray_sphere_collisions_t.t
@@ -126,46 +120,7 @@ WITH
             GROUP BY ray_id
         ) lowest_t ON ray_sphere_collisions_t.ray_id = lowest_t.ray_id AND ray_sphere_collisions_t.t = lowest_t.t
     ),
-    ray_collisions_point (ray_id, sphere_id, t, point_x, point_y, point_z) AS (
-        SELECT ray_collisions.ray_id,
-               sphere_id,
-               t,
-               rays.origin_x + rays.direction_x * t,
-               rays.origin_y + rays.direction_y * t,
-               rays.origin_z + rays.direction_z * t
-        FROM ray_collisions
-        LEFT JOIN rays ON ray_collisions.ray_id = rays.ray_id
-    ),
-    ray_collisions_outward_normal (ray_id, sphere_id, t, point_x, point_y, point_z, normal_x, normal_y, normal_z)
-        AS (
-        SELECT ray_id,
-               ray_collisions_point.sphere_id,
-               t,
-               point_x,
-               point_y,
-               point_z,
-               (point_x - spheres.center_x) / spheres.radius,
-               (point_y - spheres.center_y) / spheres.radius,
-               (point_z - spheres.center_z) / spheres.radius
-        FROM ray_collisions_point
-        LEFT JOIN spheres ON ray_collisions_point.sphere_id = spheres.sphere_id
-    ),
-    ray_collisions_is_front_face (ray_id, sphere_id, t, point_x, point_y, point_z, normal_x, normal_y, normal_z,
-                                  is_front_face) AS (
-        SELECT ray_collisions_outward_normal.ray_id,
-               sphere_id,
-               t,
-               point_x,
-               point_y,
-               point_z,
-               normal_x,
-               normal_y,
-               normal_z,
-               rays.direction_x * normal_x + rays.direction_y * normal_y + rays.direction_z * normal_z < 0.0
-        FROM ray_collisions_outward_normal
-        LEFT JOIN rays ON ray_collisions_outward_normal.ray_id = rays.ray_id
-    ),
-    ray_collisions_normal (ray_id, sphere_id, t, point_x, point_y, point_z, normal_x, normal_y, normal_z, is_front_face)
+    ray_collisions (ray_id, sphere_id, t, point_x, point_y, point_z, normal_x, normal_y, normal_z, is_front_face)
         AS (
         SELECT ray_id,
                sphere_id,
@@ -177,25 +132,49 @@ WITH
                IF(is_front_face, normal_y, -normal_y),
                IF(is_front_face, normal_z, -normal_z),
                is_front_face
-        FROM ray_collisions_is_front_face
+        FROM (
+            SELECT *,
+                   ray_direction_x * normal_x + ray_direction_y * normal_y + ray_direction_z * normal_z <
+                   0.0 AS is_front_face
+            FROM (
+                SELECT ray_collisions_point.*,
+                       (point_x - spheres.center_x) / spheres.radius AS normal_x,
+                       (point_y - spheres.center_y) / spheres.radius AS normal_y,
+                       (point_z - spheres.center_z) / spheres.radius AS normal_z
+                FROM (
+                    SELECT rays.*,
+                           sphere_id,
+                           t,
+                           ray_origin_x + ray_direction_x * t AS point_x,
+                           ray_origin_y + ray_direction_y * t AS point_y,
+                           ray_origin_z + ray_direction_z * t AS point_z
+                    FROM ray_collisions_closest
+                    LEFT JOIN rays ON ray_collisions_closest.ray_id = rays.ray_id
+                ) AS ray_collisions_point
+                LEFT JOIN spheres ON ray_collisions_point.sphere_id = spheres.sphere_id
+            ) AS ray_collisions_outward_normal
+        ) AS ray_collisions_is_front_face
     ),
     ray_hit_records (pixel_x, pixel_y, ray_direction_y, hit, normal_x, normal_y, normal_z) AS (
         SELECT pixel_x,
                pixel_y,
-               direction_y,
+               ray_direction_y,
                t IS NOT NULL,
                normal_x,
                normal_y,
                normal_z
         FROM rays
-        LEFT OUTER JOIN ray_collisions_normal ON rays.ray_id = ray_collisions_normal.ray_id
+        LEFT OUTER JOIN ray_collisions ON rays.ray_id = ray_collisions.ray_id
     ),
     ray_colors (pixel_x, pixel_y, r, g, b) AS (
         SELECT pixel_x,
                pixel_y,
-               IF(hit, (normal_x + 1.0) * 0.5, 1.0 * (1.0 - ((ray_direction_y + 1.0) * 0.5)) + 0.3 * ((ray_direction_y + 1.0) * 0.5)),
-               IF(hit, (normal_y + 1.0) * 0.5, 1.0 * (1.0 - ((ray_direction_y + 1.0) * 0.5)) + 0.5 * ((ray_direction_y + 1.0) * 0.5)),
-               IF(hit, (normal_z + 1.0) * 0.5, 1.0 * (1.0 - ((ray_direction_y + 1.0) * 0.5)) + 0.8 * ((ray_direction_y + 1.0) * 0.5))
+               IF(hit, (normal_x + 1.0) * 0.5,
+                  1.0 * (1.0 - ((ray_direction_y + 1.0) * 0.5)) + 0.3 * ((ray_direction_y + 1.0) * 0.5)),
+               IF(hit, (normal_y + 1.0) * 0.5,
+                  1.0 * (1.0 - ((ray_direction_y + 1.0) * 0.5)) + 0.5 * ((ray_direction_y + 1.0) * 0.5)),
+               IF(hit, (normal_z + 1.0) * 0.5,
+                  1.0 * (1.0 - ((ray_direction_y + 1.0) * 0.5)) + 0.8 * ((ray_direction_y + 1.0) * 0.5))
         FROM ray_hit_records
     ),
     pixels (x, y, r, g, b) AS (
